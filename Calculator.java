8c19fa98578a2ab5efade5c8d8ff6a55ba981642
benjaminsuclle <msuclles@unsa.edu.pe>
2022-04-28 19:00:52 -0500

    agregue el try de la divion para evitar el cero en el denominador

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,10 @@ class Calculator {
     int add(int a, int b){ return 0; }
     int sub(int a, int b){ return 0; }
     int mul(int a, int b){ return 0; }
-    int div(int a, int b){ return 0; }
-    int mod(int a, int b){ return 0; }
+	int div(int a, int b){
+		try {
+			return a / b; 
+		}
+	}
+	int mod(int a, int b){ return 0; }
 }
