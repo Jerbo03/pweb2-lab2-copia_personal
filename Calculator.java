8559ab76fadce4650b162d8e005d320ecf011d46
benjaminsuclle <msuclles@unsa.edu.pe>
2022-04-28 19:09:42 -0500

    agregamos el catch y finally de la excepcion y asi queda culminada

--- Calculator.java
+++ Calculator.java
@@ -5,6 +5,10 @@ class Calculator {
 	int div(int a, int b){
 		try {
 			return a / b; 
+		}catch(Exception exception){
+			System.out.println("No puede dividir entre cero\nError "+ exception.getMessage());
+		}finally{
+			return 0;
 		}
 	}
 	int mod(int a, int b){ return 0; }
