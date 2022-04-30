aaef1326eea55e4157ab8390ac443ad86f9a038e
José André Paredes Quispe <90292121+Jerbo03@users.noreply.github.com>
2022-04-29 22:05:57 -0500

    Se añadió otro método de prueba

--- holaMundo.java
+++ holaMundo.java
@@ -2,4 +2,7 @@ class HolaMundo (){
   public static void saludar() {
     System.out.println("Hola mundo");
   }
+  public static void saludar(String nombre) {
+    System.out.println("Hola " + nombre);
+  }
 }
